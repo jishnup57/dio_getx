a7abe0b86706a39e22aa973cec53e961ae90eecf
MushthakVP <mail.musthak@gmai.com>
2022-08-25 14:47:55 +0530

    get

--- lib/app/home_screen/controller/post_controller.dart
+++ lib/app/home_screen/controller/post_controller.dart
@@ -1,7 +1,27 @@
 import 'package:demo_api/app/home_screen/model/post_models.dart';
 import 'package:get/get.dart';
+import '../../service/dio_service.dart';
 
 class PostController extends GetxController {
-  String apiLink = 'https://jsonplaceholder.typicode.com/posts';
-  RxList<PostModel> posta = RxList();
+  @override
+  void onInit() {
+    getPosts();
+    super.onInit();
+  }
+
+  var url = 'https://jsonplaceholder.typicode.com/posts';
+
+  RxList<PostModel> posts = RxList();
+
+  getPosts() async {
+    var response = await DioService().getMethod(url: url);
+
+    if (response.statusCode == 200) {
+      response.data.forEach(
+        (element) {
+          posts.add(PostModel.fromJson(element));
+        },
+      );
+    }
+  }
 }
